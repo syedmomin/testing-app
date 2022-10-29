a586c337745e27d84ef6f55136f0a20efa833015
momin_finspace <s.momin@finspacegroup.co.uk>
2022-10-29 10:30:22 +0500

    new changes

--- src/index.js
+++ src/index.js
@@ -10,86 +10,37 @@ let FirstFun = ({ a, b }) => (
 )
 
 
-// let SeondFun = (props) => {
+let SeondFun = (props) => {
 
-//     const Addnumber = () => {
-//         const [setnumber, getnumber] = useState({
-//             one: "",
-//             two: ""
-//         })
-//     }
+    const Addnumber = () => {
+        const [setnumber, getnumber] = useState({
+            one: "",
+            two: ""
+        })
+    }
 
-//     const handleSubmit = (e) => {
-//         e.preventDefault()
-//         console.log(e)
-//     }
-
-//     return (
-//         <>
-//             <FirstFun a="3" b="3" />
-//             <form>
-//                 <input type="text" />
-//                 <input type="text" />
-//                 <button type='submit'>submit</button>
-//             </form>
-//         </>
-//     )
-
-// }
-
-let SimpleForm =(props) => {
-    //create object state
-    const [form, setForm] = useState({
-        FirstName: '',
-        LastName: '',
-        age: '',
-    });
-
-    const handleChange = event => {
-        event.preventDefault();
-        setForm({
-            ...form,
-            [event.target.name]: event.target.value,
-        });
-    };
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(e)
+    }
 
     return (
-        <form>
-            <label>
-                First Name:
-                <input
-                    type="text"
-                    name="firstName"
-                    value={form.firstName}
-                    onChange={handleChange}
-                />
-            </label>
-            <label>
-                Last Name:
-                <input
-                    type="text"
-                    name="lastName"
-                    value={form.lastName}
-                    onChange={handleChange}
-                />
-            </label>
-            <label>
-                Age:
-                <input
-                    type="number"
-                    name="age"
-                    value={form.age}
-                    onChange={handleChange}
-                />
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
-    );
+        <>
+            <FirstFun a="3" b="3" />
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <button type='submit'>submit</button>
+            </form>
+        </>
+    )
+
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <SimpleForm />
+     <SeondFun />
   </React.StrictMode>
 );
