79f6003a45e064169d52e388271088d109bcc66b
momin_finspace <s.momin@finspacegroup.co.uk>
2022-10-29 10:57:10 +0500

    add number

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React ,{useState}from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
@@ -12,35 +12,61 @@ let FirstFun = ({ a, b }) => (
 
 let SeondFun = (props) => {
 
-    const Addnumber = () => {
-        const [setnumber, getnumber] = useState({
-            one: "",
-            two: ""
-        })
-    }
 
+
+    // Add = () => {
+    //     var n1 = parseInt(this.state.Num1);
+    //     var n2 = parseInt(this.state.Num2);
+    //     var ans = n1 + n2;
+    // }
     const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(e)
-    }
+            e.preventDefault()
+            console.log(e)
+        }
 
     return (
-        <>
-            <FirstFun a="3" b="3" />
-            <form>
-                <input type="text" />
-                <input type="text" />
-                <button type='submit'>submit</button>
+        <div>
+            <form onSubmit={handleSubmit}>
+            <input type="text"
+                onChange={Num1 => this.setState({ Num1 })}
+            />
+            <input type="text"
+                onChange={Num2 => this.setState({ Num2 })}
+            />
+            <button type="submit">Submit data</button>
             </form>
-        </>
-    )
+        </div>
+    );
+
+// const Addnumber = () => {
+//     const [setnumber, getnumber] = useState({
+//         one: "",
+//         two: ""
+//     })
+// }
+
+// const handleSubmit = (e) => {
+//     e.preventDefault()
+//     console.log(e)
+// }
+
+// return (
+//     <>
+//         <FirstFun a="3" b="3" />
+//         <form>
+//             <input type="text" />
+//             <input type="text" />
+//             <button type='submit'>submit</button>
+//         </form>
+//     </>
+// )
 
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-     <SeondFun />
-  </React.StrictMode>
+    <React.StrictMode>
+        <SeondFun />
+    </React.StrictMode>
 );
