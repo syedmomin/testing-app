aa5a04f5922db23fecfb606c4070ce441dd91607
momin_finspace <s.momin@finspacegroup.co.uk>
2022-10-29 10:09:11 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,95 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let FirstFun = ({ a, b }) => (
+    <>
+        <h1>first function same component</h1>
+        <h3>Add number is : {parseInt(a) + parseInt(b)} </h3>
+    </>
+)
+
+
+// let SeondFun = (props) => {
+
+//     const Addnumber = () => {
+//         const [setnumber, getnumber] = useState({
+//             one: "",
+//             two: ""
+//         })
+//     }
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault()
+//         console.log(e)
+//     }
+
+//     return (
+//         <>
+//             <FirstFun a="3" b="3" />
+//             <form>
+//                 <input type="text" />
+//                 <input type="text" />
+//                 <button type='submit'>submit</button>
+//             </form>
+//         </>
+//     )
+
+// }
+
+let SimpleForm =(props) => {
+    //create object state
+    const [form, setForm] = useState({
+        FirstName: '',
+        LastName: '',
+        age: '',
+    });
+
+    const handleChange = event => {
+        event.preventDefault();
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    return (
+        <form>
+            <label>
+                First Name:
+                <input
+                    type="text"
+                    name="firstName"
+                    value={form.firstName}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>
+                Last Name:
+                <input
+                    type="text"
+                    name="lastName"
+                    value={form.lastName}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>
+                Age:
+                <input
+                    type="number"
+                    name="age"
+                    value={form.age}
+                    onChange={handleChange}
+                />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <SimpleForm />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
