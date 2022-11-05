55b8531f323d4420575c624b2a66be5e475cd009
momin_finspace <s.momin@finspacegroup.co.uk>
2022-11-05 10:27:53 +0500

    complete todo list

---
+++ src/Component/getAPI/GetAPI.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './GetAPI.css'
+
+
+const GetAPI = () => {
+
+
+
+    return (
+        <>
+        <div className="mainDiv">
+            get api data 
+        </div>
+        </>
+    )
+}
+
+export default GetAPI
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import AddNumber from './Component/add-number/AddNumber'
 import Calculator from './Component/calculator/Calculator';
+import TodoList from './Component/todoList/TodoList';
+import GetAPI from './Component/getAPI/GetAPI'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-     <Calculator/>
+     <GetAPI/>
     </React.StrictMode>
 );
