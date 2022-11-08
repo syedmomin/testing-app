5c0433026996579b246d50a6292313a827ff5d82
Syed Momin <s.momin@finspacegroup.co.uk>
2022-11-08 17:29:41 +0500

    complete two style

--- src/Component/add-number/AddNumber.jsx
+++ src/Component/add-number/AddNumber.jsx
@@ -8,7 +8,7 @@ let AddNumber = () => {
         numOne: '',
         numTwo: ''
     });
-    const total = inputs.numOne  + inputs.numTwo;
+    const total = inputs.numOne + inputs.numTwo;
 
     const handleChange = (e) => {
         const name = e.target.name;
@@ -16,21 +16,23 @@ let AddNumber = () => {
         setInputs(values => ({ ...values, [name]: value }))
     }
 
-    
+
     return (
-        <div className='mainDiv'>
-            <h4>Add Two Number : {total== NaN ? null: total}</h4>
-                <input
-                    type="text"
-                    name="numOne"
-                    onChange={handleChange}
-                />
-                <br />
-                <input
-                    type="text"
-                    name="numTwo"
-                    onChange={handleChange}
-                />  
+        <div className='add-number'>
+            <label>Auto Input Number Add</label>
+            <br />
+            <input
+                type="number"
+                name="numOne"
+                onChange={handleChange}
+            />
+            <br />
+            <input
+                type="number"
+                name="numTwo"
+                onChange={handleChange}
+            />
+            <h4>Result : {total == NaN ? null : total}</h4>
         </div>
     );
 
