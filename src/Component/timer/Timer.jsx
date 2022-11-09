a1d2b7d1aa4a18c96e000e7d84ac811e5208c543
momin_finspace <s.momin@finspacegroup.co.uk>
2022-11-09 10:31:16 +0500

    digital clock

---
+++ src/Component/timer/Timer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Countimer = () => {
+
+    return (
+        <>
+            <h1>eewrwrew</h1>
+        </>
+    );
+}
+
+export default Countimer
---
+++ src/Component/watch/Watch.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Digiwatch = () => {
+
+    return (
+        <>
+            <h1>eewrwrew</h1>
+        </>
+    );
+}
+
+export default Digiwatch
