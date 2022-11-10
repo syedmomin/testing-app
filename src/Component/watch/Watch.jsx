43aad25d78000707e1b57a00fc325c2495da0204
momin_finspace <s.momin@finspacegroup.co.uk>
2022-11-11 00:03:34 +0500

    finish watch

--- src/Component/watch/Watch.jsx
+++ src/Component/watch/Watch.jsx
@@ -1,12 +1,12 @@
-import { useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import './Watch.css'
 
 const Digiwatch = () => {
   const [date, setDate] = useState(new Date());
-  // const [today, setToday] = useState({
-  //   todayName : "",
-  //   thisMonth : ""
-  // });
+  const [today, setToday] = useState({
+    todayName: "",
+    thisMonth: ""
+  });
   const dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thuesday', 'Friday', 'Saturday'];
 
 
@@ -14,18 +14,28 @@ const Digiwatch = () => {
     setDate(new Date());
   }
   useEffect(() => {
-    // setToday({
-    //   todayName:dayName[date.getDay()],
-    //   thisMonth:"dsdfdsf" 
-    // })
+    setToday({
+      todayName: dayName[date.getDay()],
+      thisMonth: date.getMonth(),
+      thisYear: date.getFullYear()
+    })
     // (dayName[date.getDay()]);
     setInterval(refreshClock, 1000);
   }, []);
 
   return (
     <div className='mainWatch'>
-      {/* <span>{today}</span> */}
+      <div className='watchHead'>
+        <div>
+          <h1>{today.todayName}</h1>
+        </div>
+        <div>
+          <h4>-{today.thisMonth}-{today.thisYear}</h4>
+        </div>
+      </div>
+      <div className='ChangeTimer'>
       <span>{date.toLocaleTimeString()}</span>
+      </div>
     </div>
   );
 }
