bdde01d3bf26ea00d9810c1ba1832ed635db6b00
momin_finspace <s.momin@finspacegroup.co.uk>
2022-11-11 08:59:08 +0500

    timer css

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import TodoList from './Component/todoList/TodoList';
 import Digiwatch from './Component/watch/Watch';
 import Countimer from './Component/timer/Timer';
 import GetAPI from './Component/getAPI/GetAPI'
+import BasicReact from './Component/basic/Basic'
 
 const App = () => {
     return (
@@ -49,6 +50,7 @@ const App = () => {
             </header>
             <Routes>
                 <Route path="/testing-app" element={<Home />} />
+                <Route path="/basic" element={<BasicReact />} />
                 <Route path="/number" element={<AddNumber />} />
                 <Route path="/getapi" element={<GetAPI />} />
                 <Route path="/calculator" element={<Calculator />} />
---
+++ src/Component/basic/Basic.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Basic.css'
+const BasicReact = () => {
+
+    return (
+        <>
+            <h3>fregment,Jsx,Set,props,map,foreach,parent to child........</h3>
+        </>
+    )
+}
+
+export default BasicReact
