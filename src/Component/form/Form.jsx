624795fbcd4574c2df07a4f33f4a68ac36a025b0
momin_finspace <s.momin@finspacegroup.co.uk>
2022-11-17 09:29:09 +0500

    formik validation

--- src/Component/form/Form.jsx
+++ src/Component/form/Form.jsx
@@ -3,7 +3,7 @@ import './Form.css'
 import { useFormik } from 'formik';
 
 
-const NormalForm = () => {
+export const NormalForm = () => {
 
     const formik = useFormik({
         initialValues: {
@@ -23,7 +23,7 @@ const NormalForm = () => {
                 <form onSubmit={formik.handleSubmit}>
                     <label htmlFor="firstName">First Name</label>
                     <input
-                        id="firstName"
+                        name="firstName"
                         type="text"
                         className='inputField'
                         onChange={formik.handleChange}
@@ -32,7 +32,7 @@ const NormalForm = () => {
                     <br />
                     <label htmlFor="lastName">Last Name</label>
                     <input
-                        id="lastName"
+                        name="lastName"
                         type="text"
                         className='inputField'
                         onChange={formik.handleChange}
@@ -42,7 +42,7 @@ const NormalForm = () => {
 
                     <label htmlFor="email">Email Address</label>
                     <input
-                        id="email"
+                        name="email"
                         type="email"
                         className='inputField'
                         onChange={formik.handleChange}
@@ -58,14 +58,89 @@ const NormalForm = () => {
     )
 }
 
-const ValidationForm = () => {
-
+export const ValidationForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            contact: '',
+            email: '',
+            password: '',
+            conform_password: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
 
     return (
-        <>
-            <h1>hello valid</h1>
+             <>
+            <div className='fromdiv'>
+                <h1>Formik & YUP Form Validation</h1>
+                <form onSubmit={formik.handleSubmit}>
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                        name="firstName"
+                        type="text"
+                        className='inputField'
+                        onChange={formik.handleChange}
+                        value={formik.values.firstName}
+                    />
+                    <br />
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                        name="lastName"
+                        type="text"
+                        className='inputField'
+                        onChange={formik.handleChange}
+                        value={formik.values.lastName}
+                    />
+                    <br />
+
+                    <label htmlFor="contact">Contact</label>
+                    <input
+                        name="contact"
+                        type="number"
+                        className='inputField'
+                        onChange={formik.handleChange}
+                        value={formik.values.contact}
+                    />
+                    <br />
+
+                    <label htmlFor="email">Email Address</label>
+                    <input
+                        name="email"
+                        type="email"
+                        className='inputField'
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                    <br />
+
+                    <label htmlFor="password">Password</label>
+                    <input
+                        name="password"
+                        type="password"
+                        className='inputField'
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                    />
+                    <br />
+
+                    <label htmlFor="conform_password">Confirm Password</label>
+                    <input
+                        name="conform_password"
+                        type="password"
+                        className='inputField'
+                        onChange={formik.handleChange}
+                        value={formik.values.conform_password}
+                    />
+                    <br />
+
+                    <button type="submit"
+                        className='formResult'>Submit</button>
+                </form>
+            </div>
         </>
     )
 }
-
-export { NormalForm, ValidationForm }
